4e4e32853ab7850f532ff6563188df46b393ee21
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2019-02-27 22:30:02 +0200

    Add project files.

---
+++ Problem 1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Given a list of numbers and a number k, return whether any two
+                numbers from the list add up to k.
+
+                For example, given [10, 15, 3, 7] and k of 17, return true
+                since 10 + 7 is 17.
+
+                Bonus: Can you do this in one pass?
+             */
+
+            var numbers = new List<int>();
+
+            Console.WriteLine("Enter numbers (to finish - enter 0)");
+            var number = 0;
+
+            do
+            {
+                number = Convert.ToInt32(Console.ReadLine());
+                if (number != 0)
+                {
+                    numbers.Add(number);
+                }
+            } while (number != 0);
+
+            Console.WriteLine("Enter number to check:");
+            var k = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Checking {0} against: ", k);
+
+            foreach (var n in numbers)
+            {
+                Console.Write(n + " ");
+            }
+
+            Console.WriteLine();
+
+            var p = new Program();
+            var answer = p.Check(numbers, k);
+            Console.WriteLine(answer ? "True" : "False");
+        }
+
+        public bool Check(List<int> numbers, int k)
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                for (int j = i + 1; j < numbers.Count; j++)
+                {
+                    if (numbers[i] + numbers[j] == k)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Problem 2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Given an array of integers, return a new array such that each element at
+                index i of the new array is the product of all the numbers in the original
+                array except the one at i.
+
+                For example, if our input was [1, 2, 3, 4, 5], the expected output
+                would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected
+                output would be [2, 3, 6].
+
+                Follow-up: what if you can't use division?
+             */
+
+            var p = new Program();
+
+            var numbers = new List<int>();
+            var number = 0;
+
+            Console.WriteLine("Enter numbers (to finish - input 0)");
+
+            do
+            {
+                number = Convert.ToInt32(Console.ReadLine());
+                if (number != 0)
+                {
+                    numbers.Add(number);
+                }
+            } while (number != 0);
+
+            p.PrintList("Entered numbers", numbers);
+
+            var newNumbers = p.Calculate(numbers);
+            p.PrintList("Calculated numbers", newNumbers);
+        }
+
+        public void PrintList(string comment, List<int> list)
+        {
+            Console.Write(comment + ": ");
+
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+        public List<int> Calculate(List<int> originalData)
+        {
+            var newData = new List<int>();
+
+            for (int i = 0; i < originalData.Count; i++)
+            {
+                var calculatedNumber = 1;
+
+                for (int j = 0; j < originalData.Count; j++)
+                {
+                    if (i != j)
+                    {
+                        calculatedNumber *= originalData[j];
+                    }
+                }
+
+                newData.Add(calculatedNumber);
+            }
+
+            return newData;
+        }
+    }
+}
